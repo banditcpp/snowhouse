bb6b5110a93fb961538ab5e5ce822469211983ea
Joakim Karlsson <joakim@jkarlsson.com>
2014-05-10 21:48:26 +0200

    chore: added script for compiling with different configurations

---
+++ cross_compile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+STATUS=""
+
+function build_for {
+  CC=$1
+  CXX=$2
+
+  BUILD_DIR=build-$CC-CXX
+  mkdir $BUILD_DIR
+  pushd $BUILD_DIR
+  CC=$CC CXX=$CXX cmake -Dcfg=C++ ../..
+  make
+  STATUS="$STATUS\n$BUILD_DIR - Status: $?"
+  popd
+
+  BUILD_DIR=build-$CC-CXX11
+  mkdir $BUILD_DIR
+  pushd $BUILD_DIR
+  CC=$CC CXX=$CXX cmake -Dcfg=C++11 ../..
+  make
+  STATUS="$STATUS\n$BUILD_DIR - Status: $?"
+  popd
+
+}
+
+if [[ -d builds ]]; then
+  rm -rf builds
+fi
+
+mkdir builds
+pushd builds
+
+
+build_for gcc-4.5 g++-4.5
+build_for gcc-4.6 g++-4.6
+build_for gcc-4.7 g++-4.7
+build_for gcc-4.8 g++-4.8
+build_for gcc-4.9 g++-4.9
+build_for clang clang++
+popd
+
+echo "============================================"
+echo -e $STATUS
