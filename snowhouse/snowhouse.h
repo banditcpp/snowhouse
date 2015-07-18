34d4b6df949356bcc0814f976f015ebee6ced9b3
Joakim Karlsson <joakim@jkarlsson.com>
2015-07-18 14:25:53 +0200

    Version 2.1.0

--- snowhouse/snowhouse.h
+++ snowhouse/snowhouse.h
@@ -1,7 +1,7 @@
 #ifndef _SNOWHOUSE_H_JK_2013_06_28
 #define _SNOWHOUSE_H_JK_2013_06_28
 
-#define SNOWHOUSE_VERSION "2.0.1"
+#define SNOWHOUSE_VERSION "2.1.0"
 
 #if __cplusplus > 199711L
 #ifdef _MSC_VER
