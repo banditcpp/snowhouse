7abcb906adae6c30a6cae7585913da764f134e7f
Stephan Beyer <s-beyer@gmx.net>
2016-11-26 00:33:49 +0100

    Extract macro definitions to macros.h

--- snowhouse/snowhouse.h
+++ snowhouse/snowhouse.h
@@ -1,19 +1,5 @@
-#ifndef _SNOWHOUSE_H_JK_2013_06_28
-#define _SNOWHOUSE_H_JK_2013_06_28
-
-#define SNOWHOUSE_VERSION "2.1.0"
-
-#if __cplusplus > 199711L
-#ifdef _MSC_VER
-// Visual Studio (including 2013) does not support the noexcept keyword
-#define _ALLOW_KEYWORD_MACROS
-#define noexcept
-#endif
-#endif
-
-#if __cplusplus > 199711L || (defined(_MSC_VER) && _MSC_VER >= 1600)
-# define SNOWHOUSE_HAS_NULLPTR
-#endif
+#ifndef SNOWHOUSE_H
+#define SNOWHOUSE_H
 
 #include <iostream>
 #include <map>
@@ -24,6 +10,7 @@
 #include <memory>
 #include <algorithm>
 
+#include "macros.h"
 #include "stringize.h"
 #include "constraints/constraints.h"
 #include "fluent/fluent.h"
