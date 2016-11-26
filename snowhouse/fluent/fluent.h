0f2223806dbd051cf2fa9423dd4f727b92f3885e
Stephan Beyer <s-beyer@gmx.net>
2016-11-26 01:22:30 +0100

    Make fluent/constraintadapter.h self-sufficient

--- snowhouse/fluent/fluent.h
+++ snowhouse/fluent/fluent.h
@@ -1,4 +1,3 @@
-
 //          Copyright Joakim Karlsson & Kim Gräsman 2010-2012.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
@@ -7,7 +6,6 @@
 #ifndef SNOWHOUSE_FLUENT_H
 #define SNOWHOUSE_FLUENT_H
 
-#include "constraintlist.h"
 #include "constraintadapter.h"
 #include "operators/constraintoperator.h"
 #include "operators/andoperator.h"
