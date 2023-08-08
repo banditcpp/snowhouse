7cbcdbfb4aa86c511793384697f2b5a3acf225bd
Stephan Beyer <s-beyer@gmx.net>
2023-08-08 18:53:08 +0200

    Add C++20 and C++23 builds

--- util/build.sh
+++ util/build.sh
@@ -6,10 +6,10 @@ test -n "$CXX" || CXX=c++
 
 cxxstandard="$1"
 case "$cxxstandard" in
-11|14|17)
+11|14|17|20|23)
 	;;
 *)
-	echo "Usage: $0 (11|14|17) [image name]" >&2
+	echo "Usage: $0 (11|14|17|20|23) [image name]" >&2
 	exit 1
 esac
 
