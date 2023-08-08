a1caa2699e19ea93cb4aeea5582a7a060d489f97
Stephan Beyer <s-beyer@gmx.net>
2023-08-08 18:21:35 +0200

    Update CI to recent environments and improve doc of build-in-container.sh

--- util/build-in-container.sh
+++ util/build-in-container.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-if test "$#" -ne 2
-then
-	echo "Usage: $0 (11|14|17) <docker image>" >&2
+if test "$#" -ne 2; then
+	cat >&2 <<EOF
+Usage: $0 <C++ standard version> <docker image>
+
+C++ standard versions are 11, 14, 17, etc.
+
+Our docker images are, for example,
+ - banditcpp/build-environments:gcc-13
+ - banditcpp/build-environments:clang-16
+EOF
 	exit 2
 fi
 
+# run the image
 SRCDIR=/usr/src/snowhouse
 docker run \
-       -v "$PWD":"$SRCDIR" \
-       -e "SRCDIR=$SRCDIR" \
-       -e "CC=$CC" \
-       -e "CXX=$CXX" \
-       "$2" "$SRCDIR"/util/build.sh "$1" "$2"
+	-v "$PWD":"$SRCDIR" \
+	-e "SRCDIR=$SRCDIR" \
+	-e "CC=$CC" \
+	-e "CXX=$CXX" \
+	"$2" "$SRCDIR"/util/build.sh "$1" "$2"
